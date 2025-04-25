c3d1bc971746d102f6b215d8b43e62dde0983f70
doaaaliahmed <doaaali3@yahoo.com>
2025-04-25 23:25:13 +0300

    add login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import {
   Component,
 } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { LayoutComponent } from "./layout/layout/layout.component";
+import { MessagesComponent } from "./messages/messages.component";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
   standalone: true,
-  imports :[RouterModule]
+  imports :[RouterModule,LayoutComponent, MessagesComponent]
 })
 export class AppComponent  {
 
